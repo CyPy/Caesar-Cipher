72e42c66d8c350ac2f758110dad7548cca718274
Jaychandran <jaychandran001@gmail.com>
2015-09-02 09:39:41 +0530

    add comments to caesarcipher

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,9 +1,19 @@
+"""
+Functions to encode and decode Caesar ciphers given
+the numerical key.
+If the character in the plaintext/ciphertext is not
+an alphabet, then the character is left untouched.
 
+"""
 alphabets = "abcdefghijklmnopqrstuvwxyz"
 
 __all__= ['encode']
 
 def encode(plaintext, key):
+	"""
+	Encodes the plaintext using the key provided and 
+	returns the ciphertext.
+	"""
 	plaintext = plaintext.lower()
 	ciphertext = ""
 	for i in plaintext:
@@ -15,6 +25,10 @@ def encode(plaintext, key):
 	return ciphertext
 
 def decode(ciphertext, key):
+	"""
+	Decodes the ciphertext using the key provided and
+	returns the plaintext.
+	"""
 	ciphertext = ciphertext.lower()
 	plaintext = ""
 	key = key%26
@@ -24,7 +38,6 @@ def decode(ciphertext, key):
 				finalIndex = (alphabets.find(i) - key)
 			else:
 				finalIndex = (alphabets.find(i) - key + 26)
-
 			plaintext += alphabets[finalIndex]
 		else:
 			plaintext += i
